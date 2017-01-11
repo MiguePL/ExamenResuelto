d98735c90b6237161d5afa1908053bf5f73d589b
Miguel Perez Lopez <miguepl95@gmail.com>
2017-01-11 18:44:22 +0100

    Clase2

---
+++ Assets/Script/movimientoJ1.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimientoJ1 : MonoBehaviour {
+	Rigidbody2D rb;
+	float vertical = 0f;
+	float horizontal = 0f;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//-------------------GETKEYDOWN---------------------
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			vertical = 10;
+		}
+
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			vertical = -10;
+		}
+
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			horizontal = -10;
+		}
+
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			horizontal = 10;
+		}
+
+		//-------------------GETKEYUP---------------------
+		if (Input.GetKeyUp (KeyCode.UpArrow)) {
+				vertical = 0;
+		}
+
+		if (Input.GetKeyUp (KeyCode.DownArrow)) {
+				vertical = 0;
+		}
+
+		if (Input.GetKeyUp (KeyCode.LeftArrow)) {
+				horizontal = 0;
+		}
+
+		if (Input.GetKeyUp (KeyCode.RightArrow)) {
+				horizontal = 0;
+		}
+		rb.velocity = new Vector2 (horizontal, vertical);
+	}
+}
