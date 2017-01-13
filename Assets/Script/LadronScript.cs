f5a3f8c8e0da62cc8e64f32d1a4f7430c65f05eb
Miguel Perez Lopez <miguepl95@gmail.com>
2017-01-13 17:25:09 +0100

    Alien

---
+++ Assets/Script/LadronScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LadronScript : MonoBehaviour {
+	GameObject pringao;
+	CarteraScript cartera_pringao;
+	// Use this for initialization
+	void Start () {
+		pringao = GameObject.Find ("ricardito");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			cartera_pringao = pringao.GetComponent<CarteraScript> ();
+			cartera_pringao.donacion (-5);
+		}
+	}
+}
